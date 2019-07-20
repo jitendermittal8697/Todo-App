509d688d7a994de594549d87519b1b053a0f1197
jitendermittal8697 <jitendermittal8697@gmail.com>
2019-07-20 19:23:12 +0530

    Code Refactor

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var app = express();
-var middleware = require('./middleware/generateToken');
+const express = require('express');
+const app = express();
+const controller = require('./controller/controller');
 
+// var { generateToken, verifyUser} = controller;
+// verifyUser(generateToken({data: 'Jitu'}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.get('/',(req,res)=>{
-    res.render('user/registration', {query : 'registration'});
+app.get('/', (req, res) => {
+    res.render('user/registration', {
+        query: 'registration'
+    });
 })
-app.get('/login',(req,res)=>{
-    res.render('user/login', {query : 'login'});
+app.get('/login', (req, res) => {
+    res.render('user/login', {
+        query: 'login'
+    });
 })
-app.listen(3000,function(req,res){
+
+
+
+app.listen(3000, function (req, res) {
     console.log('Server Started On Port 3000');
 })
--- controller/controller.js
+++ controller/controller.js
@@ -1,2 +1,25 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+const publicKey = fs.readFileSync('./config/public.key', 'utf-8');
+const privateKey = fs.readFileSync('./config/private.key', 'utf-8');
+
+const generateToken = function (data) {
+    let token = jwt.sign(data, privateKey, {
+        algorithm: 'RS256'
+    });
+    console.log(token);
+    return token;
+}
+const verifyUser = function (token) {
+    var legit = jwt.verify(token, publicKey, {
+        algorithm: 'RS256'
+    });
+    console.log(legit);
+    return legit;
+}
+
 module.exports = {
+    generateToken,
+    verifyUser,
 }
